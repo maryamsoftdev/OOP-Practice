b7de7fb0f182578b4dc3adab80e0f7b346f9aa7b
maryamsoftdev <nabeelahmaryaam@gmail.com>
2023-02-05 18:41:28 +0500

    adding htnl through js objects

--- index.js
+++ index.js
@@ -18,25 +18,45 @@ var myObj = {
         ,         "long": 110      
     }]  
 };  
+//how to itterate through a object:
 //using for loop
-for (let i = 0; i < myObj.people.length; i++) {    
-    console.log(myObj.people[i].firstName + " " + myObj.people[i].lastName);   
-}
-//using for each and function
-myObj.people.forEach(function (item) {
-    console.log(item.firstName + " " + item.lastName);
-})
-//using fir in loop
-for (let prop in myObj.people) {
-    console.log(prop);
-    console.log(myObj.people[prop].firstName + " " + myObj.people[prop].lastName);
-}
-console.log(Object.keys(myObj));
-console.log(Object.entries(myObj));
-console.log(Object.entries(myObj)[0]);
-//using map and fuction
-const myObj2 = myObj.people.map(function (key, index) {
-    console.log(key, index);
-    return key;
-});
-console.log(myObj2);
+//using for each loop and fuctiin
+//using map
+//usingg for in prop
+// //using for loop
+// for (let i = 0; i < myObj.people.length; i++) {    
+//     console.log(myObj.people[i].firstName + " " + myObj.people[i].lastName);   
+// }
+// //using for each and function
+// myObj.people.forEach(function (item) {
+//     console.log(item.firstName + " " + item.lastName);
+// })
+// //using fir in loop
+// for (let prop in myObj.people) {
+//     console.log(prop);
+//     console.log(myObj.people[prop].firstName + " " + myObj.people[prop].lastName);
+// }
+// console.log(Object.keys(myObj));
+// console.log(Object.entries(myObj));
+// console.log(Object.entries(myObj)[0]);
+// //using map and fuction
+// const myObj2 = myObj.people.map(function (key, index) {
+//     console.log(key, index);
+//     return key;
+// });
+// console.log(myObj2);
+
+myObj.places.forEach(function(item){
+
+    console.log(item.age);
+    //for test purpose
+    // myObj.places.forEach(function(item){
+        
+        //     console.log(item);
+        // })
+        //adding html and retrieving data from object and displaying them in html
+        let div = document.createElement('div')
+        div.innerHTML=`<h3>${item.location}`;
+        console.log(div);
+        document.querySelector('div').appendChild(div);
+    })
