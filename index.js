84fba196cc8e85b93fc64638fb06401cfcd6fd61
maryamsoftdev <nabeelahmaryaam@gmail.com>
2023-02-07 19:03:54 +0500

    understanding function constructor

--- index.js
+++ index.js
@@ -153,4 +153,24 @@
 //     const tester = new Person('Laurence', 'Svekis');
 //     console.log(tester.first);
 //     tester.greeting();
-
+//CAR CONSTRUCTOR
+const honda = new Car("Red", "Honda", "Accord", 45000, 2020);
+const mustang = new Car("Black", "Ford", "Mustang", 50000, 1965);
+ 
+function Car(color, brand, make, price, year) {
+    this.color = color;
+    this.brand = brand;
+    this.make = make;
+    this.price = price;
+    this.year = year;
+    this.wheels = 4;
+    this.drive = function () {
+        console.log(`I'm driving my ${this.year} ${this.brand}, vroom vroom `);
+    }
+    this.park = function () {
+        console.log(`Parking the ${this.make}`);
+    }
+    this.sell = function () {
+        console.log(`I want to sell my  ${this.make} for at least $${this.price*0.9} since I paid $${this.price}`);
+    }
+}
